52ccbf01e6d9a406e18d8dc49139caabe658e6d9
Marios K. Pappas <66968346+Pri3st@users.noreply.github.com>
2024-08-27 09:11:01 +0300

    Update Get-RBACRoleAssignments.ps1

--- Get-RBACRoleAssignments.ps1
+++ Get-RBACRoleAssignments.ps1
@@ -14,7 +14,7 @@ $creds = New-Object System.Management.Automation.PSCredential('<UPN>', $password
 Connect-AzAccount -Credential $creds
 #>
 
-$upn = (Get-AzContext).Account.Id
+$userUPN = (Get-AzContext).Account.Id
 $subscriptionId = (Get-AzContext).Subscription.Id
 $user = Get-AzADUser -UserPrincipalName $userUPN
 $userId = $user.Id
