79c5e02be1f922e55d7dc3cf86cae562512b3943
Marios K. Pappas <66968346+Pri3st@users.noreply.github.com>
2024-09-06 11:45:42 +0300

    Update Get-ServicePrincipalEntraRoleAssignments.ps1

--- Get-ServicePrincipalEntraRoleAssignments.ps1
+++ Get-ServicePrincipalEntraRoleAssignments.ps1
@@ -1,6 +1,8 @@
-# Script to get all Role Assignments for a Service Principal, both Direct and Indirect (via Group membership).
-# Roles assigned due to Nested Group membership are also displayed.
-# The script will throw errors when enumerating for non-existent nested Group membership. Do not take them into consideration, the final results will be valid.
+<# Script to get all Role Assignments for a Service Principal, both Direct and Indirect (via Group membership).
+Roles assigned due to Nested Group membership are also displayed.
+The script will throw errors when enumerating for non-existent nested Group membership. Do not take them into consideration, the final results will be valid.
+#>
+
 $user = Get-MgServicePrincipal | Where-Object {$_.DisplayName -like "*<SERVICE_PRINCIPAL_NAME*"}
 $userId = $user.Id
 
